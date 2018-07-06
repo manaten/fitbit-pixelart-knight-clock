6c2520bdc32522630dd3e02d13d4282c10a50abb
manaten <manaten@manaten.net>
2018-07-07 03:08:33 +0900

    feat(clockface): implement clockface.

--- app/index.js
+++ app/index.js
@@ -1,16 +1,31 @@
 import clock from "clock";
 import document from "document";
 import { preferences } from "user-settings";
+import { HeartRateSensor } from "heart-rate";
+import { battery } from "power";
+
 import * as util from "../common/utils";
 
-// Update the clock every minute
-clock.granularity = "minutes";
+const heartRateLabel = document.getElementById("heartRateLabel");
+const hrm = new HeartRateSensor();
+hrm.onreading = () => {
+  heartRateLabel.text = util.monoDigits(hrm.heartRate || 0, false);
+  hrm.stop();
+}
+function updateHartRate() {
+  hrm.start();
+}
 
-// Get a handle on the <text> element
+const batteryLevel = document.getElementById("batteryLevel");
+const batteryLabel = document.getElementById("batteryLabel");
+function updateBattery() {
+  batteryLevel.width = Math.round(battery.chargeLevel * 30 / 100);
+  batteryLevel.style.fill = chargeLevelToColor(battery.chargeLevel);
+  batteryLabel.text = util.monoDigits(battery.chargeLevel) + '%';
+}
+
 const myLabel = document.getElementById("myLabel");
-
-// Update the <text> element every tick with the current time
-clock.ontick = (evt) => {
+function updateTime(date) {
   let today = evt.date;
   let hours = today.getHours();
   if (preferences.clockDisplay === "12h") {
@@ -22,4 +37,13 @@ clock.ontick = (evt) => {
   }
   let mins = util.zeroPad(today.getMinutes());
   myLabel.text = `${hours}:${mins}`;
+}
+
+clock.granularity = "seconds";
+clock.ontick = evt => {
+  if (display.on) {
+    updateHartRate();
+    updateBattery();
+    updateTime(evt.date);
+  }
 }
