cac0b66484f8c7535f8f67d31788ec297b80df10
manaten <manaten@manaten.net>
2018-07-23 02:38:34 +0900

    feat(images): add images.

--- app/index.js
+++ app/index.js
@@ -6,44 +6,52 @@ import { battery } from "power";
 
 import * as util from "../common/utils";
 
-const heartRateLabel = document.getElementById("heartRateLabel");
-const hrm = new HeartRateSensor();
-hrm.onreading = () => {
-  heartRateLabel.text = util.monoDigits(hrm.heartRate || 0, false);
-  hrm.stop();
-}
-function updateHartRate() {
-  hrm.start();
-}
+// // heart rate
+// const heartRateLabel = document.getElementById("heartRateLabel");
+// const heartRateSensor = new HeartRateSensor();
+// heartRateSensor.onreading = () => {
+//   heartRateLabel.text = util.monoDigits(heartRateSensor.heartRate || 0, false);
+//   heartRateSensor.stop();
+// }
+// function updateHartRate() {
+//   heartRateSensor.start();
+// }
 
-const batteryLevel = document.getElementById("batteryLevel");
-const batteryLabel = document.getElementById("batteryLabel");
-function updateBattery() {
-  batteryLevel.width = Math.round(battery.chargeLevel * 30 / 100);
-  batteryLevel.style.fill = chargeLevelToColor(battery.chargeLevel);
-  batteryLabel.text = util.monoDigits(battery.chargeLevel) + '%';
-}
 
-const myLabel = document.getElementById("myLabel");
-function updateTime(date) {
-  let today = evt.date;
-  let hours = today.getHours();
-  if (preferences.clockDisplay === "12h") {
-    // 12h format
-    hours = hours % 12 || 12;
-  } else {
-    // 24h format
-    hours = util.zeroPad(hours);
-  }
-  let mins = util.zeroPad(today.getMinutes());
-  myLabel.text = `${hours}:${mins}`;
-}
+// // battery
+// const batteryLevel = document.getElementById("batteryLevel");
+// const batteryLabel = document.getElementById("batteryLabel");
+// function updateBattery() {
+//   batteryLevel.width = Math.round(battery.chargeLevel * 30 / 100);
+//   batteryLevel.style.fill = chargeLevelToColor(battery.chargeLevel);
+//   batteryLabel.text = util.monoDigits(battery.chargeLevel) + '%';
+// }
+
+
+// // time & date
+// const myLabel = document.getElementById("myLabel");
+// function updateDateTime(date) {
+//   let today = date;
+//   let hours = today.getHours();
+//   if (preferences.clockDisplay === "12h") {
+//     // 12h format
+//     hours = hours % 12 || 12;
+//   } else {
+//     // 24h format
+//     hours = util.zeroPad(hours);
+//   }
+//   let mins = util.zeroPad(today.getMinutes());
+//   myLabel.text = `${hours}:${mins}`;
+// }
+
+const img = document.getElementById("img");
 
 clock.granularity = "seconds";
 clock.ontick = evt => {
-  if (display.on) {
-    updateHartRate();
-    updateBattery();
-    updateTime(evt.date);
-  }
+  // if (display.on) {
+    img.href = "images/fitbit2.png";
+    // updateHartRate();
+    // updateBattery();
+    // updateTime(evt.date);
+  // }
 }
